cbd31c7c7f163f4628c35932a9265b2b94c0b293
Jake Wilkins <jakewilkins@github.com>
2021-08-10 14:44:16 -0700

    remove more plugins

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -43,17 +43,17 @@ return require('packer').startup({function()
 		requires = { 'junegunn/fzf.vim' },
 	}
 
-	use {
-		'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-		config = function() require('gitsigns').setup() end
-	}
+	-- use {
+	--   'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+	--   config = function() require('gitsigns').setup() end
+	-- }
 
-	use {
-		'ludovicchabant/vim-gutentags',
-		config = function()
-			vim.g['gutentags_file_list_command'] = "rg --files"
-		end,
-	}
+	-- use {
+	--   'ludovicchabant/vim-gutentags',
+	--   config = function()
+	--     vim.g['gutentags_file_list_command'] = "rg --files"
+	--   end,
+	-- }
 
 
 	use {
