dbf3355deefead1ada9aa8b7e50bc9daf88104d8
Jake Wilkins <jakewilkins@github.com>
2024-07-25 14:07:54 -0700

    change path to to send-to-pasteboard

--- config/nvim/lua/linux.lua
+++ config/nvim/lua/linux.lua
@@ -7,15 +7,15 @@ vim.api.nvim_exec([[
 " nnoremap <leader>cc :!'echo -n "%<Tab>"|pbcopy<CR>'
 function! CopyFilenameToClipboard()
   silent !clear
-  " execute "!echo " . expand("%") . " | /home/jsw/workspace/bug-free-fiesta/send-to-pasteboard"
-  let var = system("/home/jsw/workspace/bug-free-fiesta/send-to-pasteboard", expand('%'))
+  " execute "!echo " . expand("%") . " | /home/vscode/.dotfiles/codespaces-helpers/send-to-pasteboard"
+  let var = system("/home/vscode/.dotfiles/codespaces-helpers/send-to-pasteboard", expand('%'))
 endfunction
 nnoremap <leader>cp :call CopyFilenameToClipboard()<cr>
 ]], false)
 
 vim.api.nvim_exec([[
 function CopyToOSClipboard() range
-  echo system('echo '.shellescape(join(getline(a:firstline, a:lastline), "\n")).'| /usr/local/bin/pbcopy')
+  echo system('echo '.shellescape(join(getline(a:firstline, a:lastline), "\n")).'| /home/vscode/.dotfiles/codespaces-helpers/send-to-pasteboard')
 endfunction
 
 vmap <leader>c :call CopyToOSClipboard()<cr>
